2a6f95207e97fd3987894e1bb8939754b75af8b7
Blessing <bntoko@gmail.com>
2020-07-27 21:17:55 +0200

    connected to back-end

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  public email = '';
+  public name = '';
+  public message = '';
+
+  constructor(
+    private httpServe: HttpService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public submitMsg() {
+    const msgObj = {};
+
+    if (this.email) {
+      msgObj['email'] = this.email
+    } else {
+      msgObj['email'] = 'Anonymous Email';
+    }
+
+    if (this.name && this.message) {
+      msgObj['name'] = this.name;
+      msgObj['message'] = this.message;
+      msgObj['_id'] = this.getUID();
+
+      console.log('Message Object ->', msgObj)
+
+      let toJSON = JSON.stringify(msgObj);
+      console.log('Message Object in JSON ->', toJSON)
+
+      // let toBase64 = btoa(toJSON);
+
+      let encrypted = this.encryptMsg(toJSON);
+      console.log('Encrypted message ->', encrypted);
+      this.httpServe.postHttp(encrypted);
+    } else {
+      console.log('Please fill in all required parts of form')
+    }
+  }
+
+  private getUID() {
+    return Math.random().toString(36).substring(2) + Date.now().toString(36);
+  }
+
+  private encryptMsg(msg) {
+    return msg;
   }
 
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs/Observable';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  private url = 'http://localhost:3000/contact';
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  public postHttp(data) {
+    console.log('Data to post ->', data);
+    let temp = {
+      type: 'Message',
+      data: data
+    }
+    return this.http.post(this.url, temp, this.httpOptions).subscribe(data => {
+      console.log('Subscribe ->', data);
+    });
+  }
 }
