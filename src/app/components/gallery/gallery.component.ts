acdd0b7ef5f208caf3c89db4031c2615ddc94e59
Blessing <bntoko@gmail.com>
2020-10-09 14:55:27 +0200

    changed back to just receiving images in base64

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -24,48 +24,9 @@ export class GalleryComponent implements OnInit {
     this.socket.getImages();
 
     this.socket.gotImages().subscribe(data => {
-      let reader = new FileReader();
+      console.log("Data ->", data);
 
-      if (typeof (data) === "string") {
-        let result = this.mergeArrays(this.tempArray);
-        // let imgTag = document.getElementById("images");
-
-        let resultBlob = new Blob(result, { type: "image/png" });
-        console.log("Blob ->", resultBlob);
-        // let blobURL = URL.createObjectURL(resultBlob);
-
-        // this.trustedUrl = this.sanitize.bypassSecurityTrustResourceUrl(blobURL);
-
-        // console.log(this.trustedUrl);
-
-
-
-
-        // console.log(newImage);
-
-        reader.onload = () => {
-          let temp: any = reader.result;
-          console.log("Reader Result ->", temp);
-          this.httpServe.imageArr.push(temp);
-
-          // let newImage = new Image();
-          // newImage.src = temp;
-
-          // imgTag.appendChild(newImage);
-          // console.log(this.httpServe.imageArr);
-        }
-        reader.readAsDataURL(resultBlob);
-
-
-      } else {
-        let temp: any = data;
-
-        let uint8 = new Uint8Array(temp);
-        this.tempArray.push(uint8);
-      }
-
-
-      // this.httpServe.imageArr.push(data);
+      this.httpServe.imageArr.push(data);
     });
   }
 
