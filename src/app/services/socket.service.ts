b7df757d5c6f92da5b7d77aed47ffc1e58c2ef02
Blessing <bntoko@gmail.com>
2020-10-10 21:19:13 +0200

    working on audio

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -6,17 +6,28 @@ import { Socket } from 'ngx-socket-io';
 })
 export class SocketService {
 
+  private uid: any;
+
   constructor(
     private socket: Socket
   ) { }
 
   getMedia(media: string, clip?: string) {
+    let mediaObj = {};
+    this.uid = this.getUID();
+    mediaObj["uid"] = this.uid;
+
     if (clip) {
+
       console.log("Clip ->", clip);
-      this.socket.emit(media, clip);
+      console.log("UID ->", this.uid);
+
+      mediaObj["data"] = clip;
+
+      this.socket.emit(media, mediaObj);
     }
 
-    this.socket.emit(media);
+    this.socket.emit(media, mediaObj);
   }
 
   gotImages() {
@@ -29,5 +40,9 @@ export class SocketService {
 
   gotVideo() {
     return this.socket.fromEvent("videoChunk");
+  }
+
+  private getUID() {
+    return Math.random().toString(36).substring(2) + Date.now().toString(36);
   }
 }
