dafcb599abd4df31df85772e865555a62306365d
Blessing <bntoko@gmail.com>
2020-10-10 23:30:35 +0200

    commented out unused function | added code in gotVideo and gotAudio subscription

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -20,39 +20,56 @@ export class GalleryComponent implements OnInit {
     this.socket.getMedia("audio");
     // this.socket.getMedia("video");
 
-    // this.socket.gotImages().subscribe(data => {
-    //   console.log("Data ->", data);
+    this.socket.gotImages().subscribe(data => {
+      console.log("Data ->", data);
 
-    //   this.httpServe.imageArr.push(data);
-    // });
+      this.httpServe.imageArr.push(data);
+    });
 
     this.socket.gotAudio().subscribe(data => {
       let temp: any = data;
 
-      if (temp["files"]) {
-        if(temp["data"]) {
-          console.log("Both files and data are present");
-          console.log("Audio data ->", temp);
-        } else {
-          this.socket.getMedia("audio", temp["files"][0]);
+      if (temp["uid"]) {
+        if (temp["uid"] === this.socket.audioUID) {
+          console.log("this.socket.audioUID ->", this.socket.audioUID);
+
+          if (temp["files"]) {
+            this.socket.getMedia("audio", temp["files"][1]);
+          }
+
+          if (temp["data"]) {
+            console.log("Audio data ->", temp);
+          }
         }
       }
 
-      console.log("audio data ->", temp);
-      console.log("this.socket.audioUID ->", this.socket.audioUID);
     });
 
     this.socket.gotVideo().subscribe(data => {
-      console.log("video data ->", data);
+      let temp: any = data;
+
+      if (temp["uid"]) {
+        if (temp["uid"] === this.socket.videoUID) {
+          console.log("this.socket.videoUID ->", this.socket.videoUID);
+
+          if (temp["files"]) {
+            this.socket.getMedia("video", temp["files"][0]);
+          }
+
+          if (temp["data"]) {
+            console.log("Video data ->", temp);
+          }
+        }
+      }
     });
   }
 
-  private mergeArrays(arrays) {
-    let result = [];
-    arrays.forEach(array => {
-      result.push(...array);
-    });
-    return result;
-  }
+  // private mergeArrays(arrays) {
+  //   let result = [];
+  //   arrays.forEach(array => {
+  //     result.push(...array);
+  //   });
+  //   return result;
+  // }
 
 }
