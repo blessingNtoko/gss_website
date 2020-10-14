c6ddbdd6d52073c53e3ffe8555937f48a16a25ac
Blessing <bntoko@gmail.com>
2020-10-14 12:29:59 +0200

    removed more code

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -6,63 +6,23 @@ import { Socket } from 'ngx-socket-io';
 })
 export class SocketService {
 
-  public imageUID: any;
-  public audioUID: any;
-  public videoUID: any;
 
   constructor(
     private socket: Socket
   ) { }
 
-  public getMedia(media: string, clip?: string) {
+  public getMedia(media: string) {
     let mediaObj = {};
 
-    if (clip) {
-      console.log("Clip ->", clip);
-
-      mediaObj["data"] = clip;
-
-      mediaObj["uid"] = this.mediaCheck(media);
-      console.log('Media Obj with clip ->', mediaObj);
-
-      this.socket.emit(media, mediaObj);
-    } else {
-      mediaObj["uid"] = this.mediaCheck(media);
+      mediaObj["uid"] = this.getUID();
       console.log('Media Obj without clip ->', mediaObj);
 
       this.socket.emit(media, mediaObj);
-    }
+
   }
 
   public gotImages() {
     return this.socket.fromEvent("images");
-  }
-
-  public gotAudio() {
-    return this.socket.fromEvent("audioChunk");
-  }
-
-  public gotVideo() {
-    return this.socket.fromEvent("videoChunk");
-  }
-
-  private mediaCheck(media) {
-    if (media === "images") {
-      this.imageUID = this.getUID();
-      console.log("imageUID ->", this.imageUID);
-
-      return this.imageUID;
-    } else if (media === "audio") {
-      this.audioUID = this.getUID();
-      console.log("audioUID ->", this.audioUID);
-
-      return this.audioUID;
-    } else if (media === "video") {
-      this.videoUID = this.getUID();
-      console.log("videoUID ->", this.videoUID);
-
-      return this.videoUID;
-    }
   }
 
   private getUID() {
